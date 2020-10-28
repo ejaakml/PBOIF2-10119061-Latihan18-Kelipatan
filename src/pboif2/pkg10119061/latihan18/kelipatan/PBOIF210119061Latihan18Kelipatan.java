190b954563ef8c6e6654ba1fa9256798f9821b60
ejaakml <eximindotong@yahoo.com>
2020-10-28 21:40:36 +0700

    18

---
+++ src/pboif2/pkg10119061/latihan18/kelipatan/PBOIF210119061Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan18.kelipatan;
+
+/**
+ *
+ * @author Rheiza
+ * NAMA  : Rheiza Akaml R
+ * KELAS : IF2   
+ * NIM   : 10119061
+ * 
+ */
+public class PBOIF210119061Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for (double i = 3.5; i <= 35; i+=3.5){
+               System.out.println(i);
+    }
+    }   
+}
